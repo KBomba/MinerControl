b34d84c4464549b896edd5793ecead32c3c3eca5
Jeckel <jbartlett@nbtechs.com>
2015-07-13 16:29:48 -0700

    Add YAAMP clones Hashpower.co and FFPool.net

---
+++ MinerControl/PriceEntries/FFPoolPriceEntry.cs
@@ -0,0 +1,13 @@
+﻿namespace MinerControl.PriceEntries
+{
+    public class FFPoolPriceEntry : PriceEntryBase
+    {
+        public decimal FeePercent { get; set; }
+
+        public override decimal Fees
+        {
+            get { return Earn*(FeePercent/100); }
+            set { base.Fees = value; }
+        }
+    }
+}
--- MinerControl/ServiceEnum.cs
+++ MinerControl/ServiceEnum.cs
@@ -5,6 +5,8 @@
         NiceHash,
         WestHash,
         YAAMP,
+        FFPool,
+        Hashpower,
         LTCRabbit,
         WePayBTC,
         MiningPoolHub,
