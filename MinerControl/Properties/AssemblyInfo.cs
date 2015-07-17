e9967a100aa110f530db640ba79e51b4c14d8853
KBomba <king.bombadil@gmail.com>
2015-07-18 01:52:41 +0200

    Upped version number

--- MinerControl/Properties/AssemblyInfo.cs
+++ MinerControl/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("KBomba Mod Corp")]
 [assembly: AssemblyProduct("Miner Control")]
-[assembly: AssemblyCopyright("Copyright © 2014 StuffOfInterest & KBomba")]
+[assembly: AssemblyCopyright("Copyright © 2014 StuffOfInterest & KBomba & Fuzzbawls")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -35,5 +35,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.6.4.0")]
-[assembly: AssemblyFileVersion("1.6.4.0")]
+[assembly: AssemblyVersion("1.6.5.0")]
+[assembly: AssemblyFileVersion("1.6.5.0")]
